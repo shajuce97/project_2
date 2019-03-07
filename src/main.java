e598c430e52825c8b85078c6688f5c87a319c8be
pouriahhh <p.hozhabri95@gmail.com>
2019-03-07 11:01:12 +0330

    add somting

--- src/main.java
+++ src/main.java
@@ -3,10 +3,13 @@ public class main {
     public static void main ( String[] args ) {
 
         int a ;
-        a = 2 ;
-        String b = "hello shaju";
+        int d;
+        a = 20 ;
+        d=50
+        String b = "hello for second time";
         System.out.println( a + b );
         System.out.println("aaabbbb");
+
 
 
     }
