9629e1c71d1c7b5d9a80536d0b181fa0a18b05d2
shajusoni <shaju.mamad@yahoo.com>
2019-03-07 11:36:51 +0330

    branch push

--- src/main.java
+++ src/main.java
@@ -5,10 +5,11 @@ public class main {
         int a ;
         a = 800 ;
         String b = "hello shaju bye shaju";
-        int d = 10 ;
+        int d = 1320 ;
         System.out.println( a + b );
         System.out.println("aaabbbb");
         System.out.println( a / d ) ;
+        System.out.println( a * d ) ;
 
 
     }
